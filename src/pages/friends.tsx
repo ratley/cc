c976a05570d501ae00b93507fe14a601345eaff5
Bradley Inniss <bfinniss@gmail.com>
2023-04-28 02:43:22 -0400

    friends setup

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,12 +1,18 @@
 import Head from "next/head";
 
+import styles from "../styles/Friends.module.scss";
+
 export default function Friends() {
   return (
     <>
       <Head>
         <title>Friends</title>
       </Head>
-      <div></div>
+      <div className={styles.friends}>
+        <div className={styles.wrapper}>
+          <div className={styles.list}></div>
+        </div>
+      </div>
     </>
   );
 }
